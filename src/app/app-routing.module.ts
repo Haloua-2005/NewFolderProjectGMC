23653a5a2ae8d3c4e20da98c09932ae4f7aa112d
Haloua-2005 <ilovehacking25@gmail.com>
2020-08-01 21:31:28 +0200

    Minimalist syntax of a route Checkpoint

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { NgIf } from '@angular/common';
+import { NgIfComponent } from './ng-if/ng-if.component';
+import { CVsComponent } from './cvs/cvs.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path : 'ngIf', component: NgIfComponent },
+  { path : 'cv', component: CVsComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
