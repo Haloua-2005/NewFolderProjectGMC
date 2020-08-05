cb34530324cb772814da2bbe183b6a15ca32d570
Haloua-2005 <ilovehacking25@gmail.com>
2020-08-05 15:54:26 +0200

    Activate actual route Checkpoint

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { Routes, RouterModule } from '@angular/router';
 import { NgIf } from '@angular/common';
 import { NgIfComponent } from './ng-if/ng-if.component';
 import { CVsComponent } from './cvs/cvs.component';
+import { NgStyleComponent } from './Directives/ng-style/ng-style.component';
 
 
 const routes: Routes = [
   { path : 'ngIf', component: NgIfComponent },
-  { path : 'cv', component: CVsComponent }
+  { path : 'cv', component: CVsComponent },
+  { path : 'ngStyle', component: NgStyleComponent }
 ];
 
 @NgModule({
--- src/app/cvs/cvs.component.ts
+++ src/app/cvs/cvs.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { CvServiceService } from '../Services/cv-service.service';
 import { HiringServiceService } from '../Services/hiring-service.service';
 import { Person } from '../attributes/Person';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-cvs',
@@ -20,7 +21,8 @@ export class CVsComponent implements OnInit {
 
   constructor(
     private cvservice:CvServiceService,
-    private hireservice:HiringServiceService
+    private hireservice:HiringServiceService,
+    private router:Router
   ) { }
 
     push(indice) {
@@ -29,6 +31,18 @@ export class CVsComponent implements OnInit {
     }
 
   ngOnInit(): void {
+  }
+
+  Try = ''
+
+  try() {
+    const LINK=['ngStyle']
+    if (this.Try === "Style") {
+      this.router.navigate(LINK)
+    }else{
+      this.Try = "Please Retry"
+    }
+
   }
 
 }
